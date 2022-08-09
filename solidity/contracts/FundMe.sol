9cbe1245611de16163801daf6ba0884e371b70bd
Remix Workspace <>
2022-08-09 23:19:38 +0700

    Develop Fundme

--- solidity/contracts/FundMe.sol
+++ solidity/contracts/FundMe.sol
@@ -1,8 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
+
+    uint256 public minimumUsd = 50;
+    // AggregatorV3Interface internal priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+
+    constructor() {}
+
     function fund() public payable {
-        require(msg.value > 1e17, "Didn't send enough");
+        require(msg.value >= minimumUsd, "Didn't send enough");
+    }
+
+    function getPrice() public view returns(uint256) {
+        // ABI
+        // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer); 
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmout) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmoutInUsd = ethPrice * ethAmout;
+        return ethAmoutInUsd;
+    }
+
+    function withdraw() public {
+        
     }
 }
