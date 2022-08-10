e982abcbdea1f229f7076ece96ac4421278b095c
Remix Workspace <>
2022-08-11 00:06:55 +0700

    Create PriceConverter lib

--- solidity/contracts/FundMe.sol
+++ solidity/contracts/FundMe.sol
@@ -1,36 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+// import "@nomiclabs/buidler/console.log";
 
 contract FundMe {
 
     uint256 public minimumUsd = 2;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    using PriceConverter for uint256;
 
     constructor() {}
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough");
-    }
-
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer); 
-    }
-
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmout) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        // because 1eth = 1 * 1e18 wei
-        uint256 ethAmoutInUsd = (ethPrice * ethAmout) / 1000000000000000000;
-        return ethAmoutInUsd;
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+        // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 
     function withdraw() public {
