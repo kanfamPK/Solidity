51e815c2e44e2638bd7d46bc723c41da48f4c1a2
Remix Workspace <>
2022-08-11 17:21:05 +0700

    Progress on FundMe, advanced solidity

--- solidity/contracts/FundMe.sol
+++ solidity/contracts/FundMe.sol
@@ -4,23 +4,41 @@ pragma solidity ^0.8.0;
 import "./PriceConverter.sol";
 // import "@nomiclabs/buidler/console.log";
 
+error NotOwner();
+
 contract FundMe {
 
-    uint256 public minimumUsd = 2;
+    uint256 public minimumUsd = 50;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
     using PriceConverter for uint256;
+    address public owner;
 
-    constructor() {}
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        // require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        if (msg.value.getConversionRate() < minimumUsd) {
+            revert NotOwner();
+        }
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
         // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 
-    function withdraw() public {
-        
+    function withdraw() public onlyOwner {
+        for (uint8 i = 0; i < funders.length; i++) {
+            addressToAmountFunded[funders[i]] = 0;
+
+            (bool sent, bytes memory data) = payable(msg.sender).call{value: address(this).balance}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Forbidden");
+        _;
     }
 }
--- solidity/contracts/SafeMathTester.sol
+++ solidity/contracts/SafeMathTester.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
 contract SafeMathTester {
-    
+    uint8 public uint8Number = 255;
+
+    function add() public {
+         uint8Number = uint8Number + 1;
+    }
 }
