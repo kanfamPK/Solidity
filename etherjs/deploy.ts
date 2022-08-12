c751b57ed3f3c48dc08476028831896eb2ab7eec
khan <khanpham1995@gmail.com>
2022-08-13 01:21:43 +0700

    Progress on ethers

--- etherjs/deploy.ts
+++ etherjs/deploy.ts
@@ -1,8 +1,9 @@
 import { ethers } from "ethers";
 import * as fs from "fs-extra";
 import { config } from "./config";
+import * as hexConverter from 'hex2dec';
 
-async function main() {
+(async function main() {
   const provider = new ethers.providers.JsonRpcProvider(config.rpc);
   const wallet = new ethers.Wallet(config.privateKey, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -12,22 +13,25 @@ async function main() {
   );
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   const contract = await contractFactory.deploy();
-  const deploymentReceipt = await contract.deployTransaction.wait(1);
+  // const deploymentReceipt = await contract.deployTransaction.wait(1);
 
+  await contract.store("5");
+  const txFavoriteNumber = await contract.retrieve();
+  console.log('AATransaction favorite number: ', `${txFavoriteNumber}` );
+
+  // const nonce = await wallet.getTransactionCount();
   // chainId is depend on ganache on each computer, aware about that
-  const tx = {
-    nonce: 5,
-    gasPrice: 20000000000,
-    gasLimit: 1000000,
-    to: null,
-    value: 0,
-    data: "0x" + config.txExampleBinData,
-    chainId: 5777
-  };
-  const signedTx = await wallet.signTransaction(tx);
-  const sentTx = await wallet.sendTransaction(tx);
-  await sentTx.wait(1);
-  console.log('', sentTx);
-}
-
-main();
+  // const tx = {
+  //   nonce,
+  //   gasPrice: 20000000000,
+  //   gasLimit: 1000000,
+  //   to: null,
+  //   value: 0,
+  //   data: "0x" + config.txExampleBinData,
+  //   chainId: config.chainId
+  // };
+  // const signedTx = await wallet.signTransaction(tx);
+  // const sentTx = await wallet.sendTransaction(tx);
+  // await sentTx.wait(1);
+  // console.log('', sentTx);
+})()
