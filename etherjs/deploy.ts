7baaa6ede3bbdce0d3cc8efe8f7dba69bfe7a3f0
khan.pham <khan.pham@saigontechnology.com>
2022-08-15 17:30:27 +0700

    Complete ethers part

--- etherjs/deploy.ts
+++ etherjs/deploy.ts
@@ -2,22 +2,24 @@ import { ethers } from "ethers";
 import * as fs from "fs-extra";
 import { config } from "./config";
 import * as hexConverter from 'hex2dec';
+import 'dotenv/config';
 
 (async function main() {
   const provider = new ethers.providers.JsonRpcProvider(config.rpc);
-  const wallet = new ethers.Wallet(config.privateKey, provider);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log('Deploying contract.....')
   const contract = await contractFactory.deploy();
   // const deploymentReceipt = await contract.deployTransaction.wait(1);
-
+  console.log(`Deploy successfully, contract address: ${contract.address}`);
   await contract.store("5");
   const txFavoriteNumber = await contract.retrieve();
-  console.log('AATransaction favorite number: ', `${txFavoriteNumber}` );
+  console.log('Transaction favorite number: ', `${txFavoriteNumber}` );
 
   // const nonce = await wallet.getTransactionCount();
   // chainId is depend on ganache on each computer, aware about that
