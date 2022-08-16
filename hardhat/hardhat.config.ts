d25a8e89cef83ac1c7ec1004fab8f04fb0521177
khan <khanpham1995@gmail.com>
2022-08-16 19:07:40 +0700

    adjust package dependencies, Init contract deployment

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-waffle";
+import("@nomiclabs/hardhat-waffle");
+import("hardhat-gas-reporter");
+import("@nomiclabs/hardhat-etherscan");
+import "dotenv/config";
+import "solidity-coverage";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { task, HardhatUserConfig } from "hardhat/config"
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/mRTunZyOSlnIcyqHdSYSic2VPSd0gpqA",
+      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+      chainId: 4
+    }
+  },
   solidity: "0.8.9",
 };
 
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,19 +1,25 @@
 import { ethers } from "hardhat";
-import "@nomiclabs/hardhat-ethers";
+import "dotenv/config";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // console.log("Lock with 1 ETH deployed to:", lock.address);
+
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying.....");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed, contract address: ${simpleStorage.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
